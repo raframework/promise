38cce930f16fd65c5bef1f60c48799f726ddbf0a
coderd <coderd.box@gmail.com>
2017-05-26 19:42:42 +0800

    Added: some packages

---
+++ src/Promise/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * User: coderd
+ * Date: 2017/5/26
+ * Time: 19:36
+ */
+
+namespace Promise;
+
+
+class Test
+{
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * User: coderd
+ * Date: 2017/5/26
+ * Time: 19:40
+ */
+
+namespace tests;
+
+class Test
+{
+
+}
